ac66ec89e742cc1d01667c7be4163ff40462cdc8
aakash-sharma25 <as024harma@gmail.com>
2024-08-31 21:26:51 +0530

    final commit

--- index.js
+++ index.js
@@ -17,10 +17,10 @@ app.use(express.json());
 app.use("/api/auth", authRoutes);
 app.use("/api/user", userRoutes);
 
-app.use(express.static(path.join(__dirname, "./client/dist")))
+app.use(express.static(path.join(__dirname, "./client/dist")));
 
-app.get( "*" , (req,res) =>{
-  res.sendFile(path.join(__dirname , "./client/dist/index.html"))
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "./client/dist/index.html"));
 });
 
 app.listen(PORT, () => {
