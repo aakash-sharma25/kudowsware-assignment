756925509999defd226aeb8ba02d271a2181462f
aakash-sharma25 <as024harma@gmail.com>
2024-08-31 21:19:01 +0530

    production commit

--- index.js
+++ index.js
@@ -17,6 +17,12 @@ app.use(express.json());
 app.use("/api/auth", authRoutes);
 app.use("/api/user", userRoutes);
 
+app.use(express.static(path.join(__dirname, "./client/dist")))
+
+app.get( "*" , (req,res) =>{
+  res.sendFile(path.join(__dirname , "./client/dist/index.html"))
+});
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
